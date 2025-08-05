f3e27dc254cfdeeae6af223eb4d2996c7e9b03b9
mdsaibhossain <saibhossain5@gmail.com>
2025-08-05 23:35:10 +0600

    update

--- app/vector_store.py
+++ app/vector_store.py
@@ -1,3 +1,10 @@
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+chunks , vectors = [],[]
+index = None
+
+def split_text(text,max_len=20):
+    return
