fa3f46ab9090a2d86af1afda8eb2b118c52e01cd
mdsaibhossain <saibhossain5@gmail.com>
2025-08-06 00:15:02 +0600

    app updates

--- app/vector_store.py
+++ app/vector_store.py
@@ -7,4 +7,15 @@ chunks , vectors = [],[]
 index = None
 
 def split_text(text,max_len=20):
-    return
+    return[text[i:i+max_len] for i in range(0,len(text),max_len)]
+
+def build_index(texts):
+    global chunks, vectors , index
+    chunks = split_text(texts)
+    vectors = model.encode(chunks)
+    index = faiss.IndexFlatL2(vectors.shape[1])
+
+def search(quary,top_k=3):
+    quary_vec = model.encode([quary])
+    D, I = index.search(quary_vec,top_k)
+    return [chunks[i] for i in I[0]]
